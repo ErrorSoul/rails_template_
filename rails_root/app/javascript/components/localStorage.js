f819b702aadcd2e2494b17ee90d1206801f3cd46
ErrorSoul <neurofag@gmail.com>
2021-04-18 23:55:24 +0300

    clean yarn packages

--- rails_root/app/javascript/components/localStorage.js
+++ rails_root/app/javascript/components/localStorage.js
@@ -1,22 +1,19 @@
-export const loadState = () => {
-		try {
-			const serializedState = localStorage.getItem('state');
-			if (serializedState === null) {
-				return undefined;
-			}
-			return JSON.parse(serializedState);
-		} catch (err) {
-			return undefined;
-		}
-}
+export const loadState = (namespaceKey) => {
+  console.log("LOAD STATE");
+  try {
+    const serializedState = localStorage.getItem(namespaceKey);
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
 
-
-export const saveState = (state) => {
-	try {
-		const serializedState = JSON.stringify(state);
-		localStorage.setItem('state', serializedState);
-
-	} catch (err) {
-
-	}
-}
+export const saveState = (state, namespaceKey) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem(namespaceKey, serializedState);
+  } catch (err) {}
+};
